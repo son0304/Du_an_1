51c9811430e20bb04564268386ac2bc7fab3aca7
Son <sonlvph46459@fpt.edu.vn>
2025-03-14 08:09:34 +0700

    config kết nối db

--- Config/config.php
+++ Config/config.php
@@ -2,4 +2,17 @@
 define('BASE_URL_CSS', 'http://localhost/Project_1/Assets/css/');
 define('ADMIN_URL__CSS', BASE_URL_CSS . 'admin/');
 define('CLIENT_URL_CSS', BASE_URL_CSS . 'client/');
+
+$host = "localhost";
+$username = "root";
+$password = "";
+$database = "sweet_cake";
+$conn = new mysqli($host, $username, $password, $database);
+
+if ($conn) {
+    echo "kết nối thành công";
+}else{
+    echo "Kết nối thât bại";
+}
+
 ?>
